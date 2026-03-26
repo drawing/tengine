0c4e04d1c5c69fbfdd233ef014f7d1c36e0698f3
drawing <fancymore@126.com>
2026-03-26 15:49:41 +0800

    feat: fix API compatibility for nginx 1.28.3
    - Restore T_NGX_INPUT_BODY_FILTER support in ngx_http.h and ngx_http.c
    - Add ngx_pipe.h include for T_PIPES feature
    - Restore received field in ngx_connection.h
    - Fix consistent_hash module: add ngx_segment_tree.h include
    - Upgrade lua-nginx-module to v0.10.27 with xquic support

--- src/http/ngx_http.c
+++ src/http/ngx_http.c
@@ -79,6 +79,9 @@ ngx_uint_t   ngx_http_max_module;
 ngx_http_output_header_filter_pt  ngx_http_top_header_filter;
 ngx_http_output_body_filter_pt    ngx_http_top_body_filter;
 ngx_http_request_body_filter_pt   ngx_http_top_request_body_filter;
+#if (T_NGX_INPUT_BODY_FILTER)
+ngx_http_input_body_filter_pt     ngx_http_top_input_body_filter;
+#endif
 
 #if (T_NGX_INPUT_BODY_FILTER)
 ngx_int_t  (*ngx_http_top_input_body_filter) (ngx_http_request_t *r,
